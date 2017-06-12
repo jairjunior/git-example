4fab951f28358ff2df0b22c1f7521abb0b92eecb
Jair Junior <contato@jairjunior.eng.br>
2017-06-12 19:40:07 -0300

    Versão final da Classe Maths testada e funcionando.

--- testbench.cpp
+++ testbench.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include "Maths.h"
 
 using namespace std;
@@ -7,5 +8,12 @@ int main(){
   double a = 11.03;
   double b = 29.47;
   Maths conta(a,b);
-  
+
+
+	cout << a <<" + "<< b <<" = " << conta.soma() << endl;
+	cout << a <<" - "<< b <<" = " << conta.subtracao() << endl;
+	cout << a <<" * "<< b <<" = " << conta.multiplicacao() << endl;
+	cout << a <<" / "<< b <<" = " << fixed << setprecision(5) << conta.divisao() << endl;
+
+	return 0;
 }
